a665bbf07c5495c98870cff623294c0f9042f35b
NunYunwjy <1493001591@qq.com>
2024-12-11 09:35:12 +0800

    添加项目文件。

---
+++ Projects/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projects.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int category_id { get; set; }
+
+        public string name { get; set; }
+        public bool status { get; set; }
+        public string description { get; set; }
+
+        [ForeignKey("parentCategory")]
+        public int? parentcategory_id { get; set; }
+        public virtual Category parentCategory { get; set; }
+
+        public int admin_id { get; set; }
+
+    }
+}
